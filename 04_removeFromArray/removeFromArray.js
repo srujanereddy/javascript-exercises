edcc3568cf91272f68d082f61c34bf58ef3287f3
Srujan <srujanereddy@gmail.com>
2025-03-02 21:47:24 +0530

    Finish excercise 4 from javascript-excercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for(arg of args){
+        while(array.indexOf(arg)>-1) array.splice(array.indexOf(arg),1);
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
