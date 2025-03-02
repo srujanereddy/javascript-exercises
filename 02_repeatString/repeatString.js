ec9c64fef2e02a4276e97ee07f88ca813f44f8de
Srujan <srujanereddy@gmail.com>
2025-03-02 20:49:41 +0530

    Finish excercise 2 from javascript-excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    let result="";
+    if(count<0) return "ERROR";
+    for(let i=0;i<count;i++){
+        result+=string;
+    }
+    return result;
 };
 
 // Do not edit below this line
