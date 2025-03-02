0cba0dbba19351053c95c7e419bbbdebeaa83687
Srujan <srujanereddy@gmail.com>
2025-03-02 21:18:03 +0530

    Finish excercise 3 from javascript-excercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    stringArray.reverse();
+    let result = stringArray.join("");
+    return result;
 };
 
 // Do not edit below this line
