e10fbab3b4b26bf64061e51edee872dca6ac07ee
Srujan <srujanereddy@gmail.com>
2025-03-16 21:53:36 +0530

    Finish excercise 12 from javascript-excercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a,b) => {
+        if(!a.yearOfDeath) a.yearOfDeath = (new Date()).getFullYear();
+        if(!b.yearOfDeath) b.yearOfDeath = (new Date()).getFullYear();
+        return (b.yearOfDeath - b.yearOfBirth)-(a.yearOfDeath - a.yearOfBirth);
+    })
+    return people[0];
 };
 
 // Do not edit below this line
