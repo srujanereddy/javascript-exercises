3be37a1e95be7aa1fa2045bdd134abc1363f11cc
Srujan <srujanereddy@gmail.com>
2025-03-16 21:07:18 +0530

    Complte excercise 9 from javascript-excercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alnum = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    str = str
+        .toLowerCase()
+        .split('')
+        .filter(char => alnum.includes(char))
+        .join('');
+    return str.split('').reverse().join('') === str;
 };
 
 // Do not edit below this line
