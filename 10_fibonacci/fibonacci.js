fc431d74dc15f0317b0243a76313affb763b6043
Srujan <srujanereddy@gmail.com>
2025-03-16 21:23:07 +0530

    Finish excercise 10 from javascript-excercices

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num= +num;
+    let a=1,b=1;
+    if(num<0)
+        return "OOPS";
+    if(num ===0)
+        return 0;
+    if(num==1 || num==2)
+        return 1;
+    num-=2;
+    while(num--){
+        fib=a+b;
+        [a,b]=[b,fib];
+    }
+    return fib;
 };
 
 // Do not edit below this line
